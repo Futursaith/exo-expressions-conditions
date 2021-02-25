cdd0a5844d461a55ea4ec5972d2f63f4d5fa344b
Futursaith <saith_7@hotmail.fr>
2021-02-25 16:19:51 +0100

    exo3

---
+++ crackme06.js
@@ -0,0 +1,10 @@
+let password = '1a2b!AZEAZEAZEAZEAZEAZEAZ'
+
+const crackme6 = (password) => {
+  if (password.length > 13 === '1', 'a', '2', 'b', '!' && password[0, 4]) {
+    msg = 'OK'
+  } else {
+    msg = 'BAD'
+  }
+}
+crackme6(password)
